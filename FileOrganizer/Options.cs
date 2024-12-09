1a1790d775e61928049a51c570a8d104724f9646
ToyinM <toyinmuhammed50@gmail.com>
2024-12-09 09:48:00 +0100

    cli v1.0.0

---
+++ FileOrganizer/Options.cs
@@ -0,0 +1,12 @@
+using CommandLine;
+
+namespace FileOrganizer;
+
+public class Options
+{
+    [Option('c', "config", Required = false, HelpText = "Set the config file")]
+    public string Config { get; set; } = string.Empty;
+    
+    [Option('d', "directory", Required = true, HelpText = "Set the target directory")]
+    public string Directory { get; set; } = string.Empty;
+}
--- FileOrganizer/Program.cs
+++ FileOrganizer/Program.cs
@@ -3,73 +3,35 @@ using FileOrganizer;
 
 public static class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         Parser.Default.ParseArguments<Options>(args)
             .WithParsed<Options>(o =>
             {
-                if (o.Verbose)
+                Console.WriteLine("Welcome to the FileOrganizer!");
+                var configs = new Configs();
+                FileInfo file = default;
+                if (!string.IsNullOrEmpty(o.Config))
                 {
-                    Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
-                    Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    file = new FileInfo(o.Config);
+                    if (!file.Exists)
+                    {
+                        Console.WriteLine(("Invalid file path."));
+                        return;
+                    }
+                    configs = configs.GetConfigs(file);
                 }
-                else
+                file = new FileInfo("config.json");
+                if (!file.Exists)
                 {
-                    Console.WriteLine($"Current Arguments: -v {o.Verbose}");
-                    Console.WriteLine("Quick Start Example!");
+                    Console.WriteLine(("Cannot find config file. Check your installation and try again."));
+                    return;
                 }
+                configs = configs.GetConfigs(file);
+                var result = Organizer.Organize(o.Directory, configs);
+                if (result >= 1) Console.WriteLine("Organizer ran successfully!"); else Console.WriteLine("Organizer failed");
+                return;
             });
-        // Define your configurations
-        var configs = new Configs().GetConfigs();
-
-        if (configs is null)
-        {
-            Console.WriteLine("Error: Failed to read config file.");
-            return;
-        }
-
-// Define target directory
-        const string directoryPath = @"C:\Users\TOYIN\Downloads\Testing";
-
-        if (!Directory.Exists(directoryPath))
-        {
-            Console.WriteLine("Invalid directory path.");
-            return;
-        }
-
-// Get all files in the directory
-        var files = Directory.GetFiles(directoryPath);
-
-        if (files.Length == 0)
-        {
-            Console.WriteLine("No files in this directory");
-            return;
-        }
-
-        foreach (var file in files)
-        {
-            FileInfo fileInfo = new FileInfo(file);
-
-            // Find matching configuration for the file extension
-            var matchingConfig = configs.Configurations.Find(config =>
-                config.Extensions.Contains(fileInfo.Extension.ToLower()));
-
-            if (matchingConfig is null)
-            {
-                Console.WriteLine($"File {fileInfo.FullName} matches no configuration.");
-                continue;
-            }
-            // Create the target directory if it doesn't exist
-            var targetDirPath = Path.Combine(directoryPath, matchingConfig.DirectoryName);
-            Directory.CreateDirectory(targetDirPath);
-
-            // Move the file
-            var targetFilePath = Path.Combine(targetDirPath, fileInfo.Name);
-            File.Move(fileInfo.FullName, targetFilePath);
-
-            Console.WriteLine($"Moved {fileInfo.Name} to {targetDirPath}");
-        }
-
-        Console.WriteLine("File organization complete.");
+        
     }
 }
