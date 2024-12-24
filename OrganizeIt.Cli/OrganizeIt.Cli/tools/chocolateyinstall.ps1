1ee58273d107479cc893515daf9c5338241ca4d6
ToyinM <toyinmuhammed50@gmail.com>
2024-12-24 15:41:27 +0100

    chocolatey package bundling

---
+++ OrganizeIt.Cli/OrganizeIt.Cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'organizeit.cli'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$binRoot = Get-ToolsLocation
+
+$targetPath = Join-Path $binRoot $packageName
+
+Install-ChocolateyZipPackage $packageName 'https://github.com/Toyin5/OrganizeIt/releases/download/v1.0.0-alpha/release.zip' $toolsDir
+
+# Create a shortcut to the executable
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\OrganizeIt.Cli.lnk" -targetPath "$targetPath\organizeit.cli.exe"
