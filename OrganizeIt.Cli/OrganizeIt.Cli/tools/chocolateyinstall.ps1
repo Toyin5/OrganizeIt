4e0eb40cb629aa6eeb3a9f4243e979a5c6810297
ToyinM <toyinmuhammed50@gmail.com>
2024-12-25 22:59:53 +0100

    updated nupkg

--- OrganizeIt.Cli/OrganizeIt.Cli/tools/chocolateyinstall.ps1
+++ OrganizeIt.Cli/OrganizeIt.Cli/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $binRoot = Get-ToolsLocation
 
 $targetPath = Join-Path $binRoot $packageName
 
-Install-ChocolateyZipPackage $packageName 'https://github.com/Toyin5/OrganizeIt/releases/download/v1.0.0-alpha/release.zip' $toolsDir
+Install-ChocolateyZipPackage $packageName 'https://github.com/Toyin5/OrganizeIt/archive/refs/tags/v.1.0.1.zip' $toolsDir
 
 # Create a shortcut to the executable
 Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\OrganizeIt.Cli.lnk" -targetPath "$targetPath\organizeit.cli.exe"
